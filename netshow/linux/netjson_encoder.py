ed11b03c046b43302c7dfa6e63b726698fa59f9d
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-04-30 08:51:55 +0000

    json encoder magic ! for printing netshow interfaces as a json string. so for works for netshow interfaces

---
+++ netshow/linux/netjson_encoder.py
@@ -0,0 +1,22 @@
+# pylint: disable=E0202
+"""
+Module for Converting python class attributes into JSON
+"""
+from json import JSONEncoder
+
+
+class NetEncoder(JSONEncoder):
+    """
+    NetEncoder is my custom JSONEncoder class that prints out
+    the json output of any PrintIface Class or subclass.
+    """
+    def default(self, obj):
+        # print out properties
+        property_names = [_p for _p in dir(obj.__class__) if isinstance(
+            getattr(obj.__class__, _p), property)]
+        _hash = {}
+        for _property in property_names:
+            _hash[_property] = getattr(obj, _property)
+        if hasattr(obj, 'iface'):
+            _hash['iface_obj'] = obj.iface
+        return _hash
