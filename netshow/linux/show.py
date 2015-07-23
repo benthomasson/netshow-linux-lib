2f05b28ff257dfe62f6e0034c5010b3b05eb431f
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-07-23 05:46:05 +0000

    netshow -V should work now

--- netshow/linux/show.py
+++ netshow/linux/show.py
@@ -79,7 +79,7 @@ def run():
         elif _nd.get('lldp'):
             _shownei = ShowNeighbors(_nd)
             print(_shownei.run())
-        elif _nd.get('--version') or _nd.get('-v'):
+        elif _nd.get('--version') or _nd.get('-V'):
             print(print_version())
         else:
             print(__doc__)
